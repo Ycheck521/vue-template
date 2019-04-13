ff1f2906d7070bd75e37dca6f9b48c343bba823b
Ycheck <17326842024@163.com>
2019-04-13 11:14:03 +0800

    封装axios

--- src/store/action.js
+++
@@ -1,9 +0,0 @@
-import { getEcosystem } from '../services'
-import { ADD_ECOSYSTEM } from './types'
-
-export default {
-  async getEcosystem ({commit, store}) {
-    let { data } = await getEcosystem()
-    commit(ADD_ECOSYSTEM, data.body)
-  }
-}
--- src/store/contacts/action.js
+++
@@ -1,9 +0,0 @@
-import { getContacts } from '../../services'
-import { UPDATE_CONTACTS } from './types'
-
-export default {
-  async getContacts ({commit, store}) {
-    let { data } = await getContacts()
-    commit(UPDATE_CONTACTS, {val: data.body})
-  }
-}
